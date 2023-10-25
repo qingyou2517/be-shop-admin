266a1e6cd83b7f09116b914389d5b4cc4c1e5bde
qingyou2517 <2517789608@qq.com>
2023-10-26 01:56:28 +0800

    feat: 项目初始化，安装依赖

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+  <button class="btn">按钮</button>
+</template>
+
+<style scoped>
+.btn {
+  @apply bg-purple-500 text-indigo-50 px-4 py-2 rounded-full hover:( bg-purple-700) transition-all duration-500 focus:(ring-2 ring-purple-900);
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,14 @@
+import { createApp } from "vue";
+// import './style.css'
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+import App from "./App.vue";
+import router from "./router";
+
+const app = createApp(App);
+app.use(ElementPlus);
+app.use(router);
+
+import "virtual:windi.css";
+
+app.mount("#app");
