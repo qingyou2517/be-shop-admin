d115796b6e279842507915ba41e23dc600fdc59f
qingyou2517 <2517789608@qq.com>
2023-11-11 06:57:00 +0800

    feat: 实现公告列表的增删改

--- src/api/notice.js
+++ src/api/notice.js
@@ -4,3 +4,15 @@ import axios from "~/axios";
 export function getNoticeList(page) {
   return axios.get(`/admin/notice/${page}`);
 }
+
+export function addNotice(data) {
+  return axios.post("/admin/notice", data);
+}
+
+export function updateNotice(id, data) {
+  return axios.post(`/admin/notice/${id}`, data);
+}
+
+export function deleteNotice(id) {
+  return axios.post(`/admin/notice/${id}/delete`);
+}
--- src/pages/notice/list.vue
+++ src/pages/notice/list.vue
@@ -1,6 +1,13 @@
 <script setup>
 import { ref, reactive } from "vue";
-import { getNoticeList } from "~/api/notice.js";
+import {
+  getNoticeList,
+  addNotice,
+  updateNotice,
+  deleteNotice,
+} from "~/api/notice.js";
+import { toast } from "~/composables/util";
+import FormDrawer from "~/components/FormDrawer.vue";
 
 const tableList = ref([]);
 const loading = ref(false);
@@ -28,14 +35,86 @@ async function getData(p = null) {
 }
 getData();
 
+// 抽屉组件的数据
+const drawerTitle = ref("");
+const formDrawerRef = ref(null);
+
+// 表单数据
+const updateId = ref(0);
+const formRef = ref(null);
+const form = reactive({
+  title: "",
+  content: "",
+});
+const rules = {
+  title: [
+    {
+      required: true,
+      message: "公告标题不能为空",
+      trigger: "blur", // 触发校验的时机是：失去焦点时
+    },
+  ],
+  content: [
+    {
+      required: true,
+      message: "公告内容不能为空",
+      trigger: "blur", // 触发校验的时机是：失去焦点时
+    },
+  ],
+};
+
+// 表单校验
+function handleSubmit() {
+  formRef.value.validate(async (valid) => {
+    if (!valid) return;
+
+    formDrawerRef.value.showLoading();
+    try {
+      if (updateId.value !== 0) {
+        await updateNotice(updateId.value, form);
+        toast("修改成功");
+        getData(currentPage.value);
+      } else {
+        await addNotice(form);
+        toast("新增成功");
+        getData(1);
+      }
+
+      formDrawerRef.value.close();
+    } finally {
+      formDrawerRef.value.hideLoading();
+    }
+  });
+}
+
 // 新增公告
-const handleAdd = () => {};
+const handleAdd = () => {
+  form.title = "";
+  form.content = "";
+  drawerTitle.value = "新增";
+  formDrawerRef.value.open();
+};
 
 // 编辑公告
-const handleEdit = () => {};
+const handleEdit = (item) => {
+  form.title = item.title;
+  form.content = item.content;
+  updateId.value = item.id;
+  drawerTitle.value = "修改";
+  formDrawerRef.value.open();
+};
 
 // 删除公告
-const handleDelete = (id) => {};
+const handleDelete = async (id) => {
+  loading.value = true;
+  try {
+    await deleteNotice(id);
+    toast("删除成功");
+    getData(currentPage.value);
+  } finally {
+    loading.value = false;
+  }
+};
 </script>
 
 <template>
@@ -59,7 +138,12 @@ const handleDelete = (id) => {};
       <el-table-column prop="create_time" label="发布时间" width="380" />
       <el-table-column label="操作" align="center">
         <template #default="scope">
-          <el-button type="primary" text size="small" @click="handleEdit">
+          <el-button
+            type="primary"
+            text
+            size="small"
+            @click="handleEdit(scope.row)"
+          >
             修改
           </el-button>
           <el-popconfirm
@@ -86,6 +170,24 @@ const handleDelete = (id) => {};
         @current-change="getData"
       />
     </div>
+
+    <!-- 页面保持一个根节点，防止切换页面时 vue transition 失效 -->
+    <FormDrawer ref="formDrawerRef" :title="drawerTitle" @submit="handleSubmit">
+      <el-form
+        :model="form"
+        ref="formRef"
+        :rules="rules"
+        label-width="100px"
+        :inline="false"
+      >
+        <el-form-item label="公告标题:" prop="title">
+          <el-input v-model="form.title"></el-input>
+        </el-form-item>
+        <el-form-item label="公告内容:" prop="content">
+          <el-input v-model="form.content"></el-input>
+        </el-form-item>
+      </el-form>
+    </FormDrawer>
   </el-card>
 </template>
 
